3c65b965bd26f8249fbd94f28f9ae9c91c5c7f80
VK's MAC MINI <vineeth.k@wacmob.com>
2018-10-14 19:41:37 +0530

    Initial Commit

---
+++ MovieSearchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSearchApp
+//
+//  Created by MAC on 10/14/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
